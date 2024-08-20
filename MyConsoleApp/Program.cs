bff4a866063e9e92f3848058f06760cae35b42e3
zbikma <z.mazadi@gmail.com>
2024-08-19 20:46:25 -0400

    initial commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Kubrenetes Console App is startin...!");
+while(true){
+    Console.WriteLine($"App is running at {DateTime.Now}");
+    Thread.Sleep(5000);
+}
